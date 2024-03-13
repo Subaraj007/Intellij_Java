5f99af71205f196a1bff99657a79c2ebcd329045
Subaraj <subaraj1961@gmail.com>
2024-03-13 08:55:05 +0530

    Swaping

---
+++ src/Swaping.java
@@ -0,0 +1,14 @@
+public class Swaping {
+    public static void main(String[] args) {
+        int a=60;
+        int b=45;
+
+        System.out.println("Before swaping: "+"a= " +a +" " +"b= " +b);
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("After swaping: " +"a= " +a +" "+"b= " +b);
+
+
+    }
+}
